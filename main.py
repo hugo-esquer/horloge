40731bb62bc5b95c77e338df036555312e80ebe4
hugo-esquer <hugo.esquer@laplateforme.io>
2023-11-28 20:06:52 +0100

    implémentation de la pause

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import keyboard
 
 def afficher_heure(t):
     global heures
@@ -8,24 +9,19 @@ def afficher_heure(t):
     minutes = t[1]
     secondes = t[2]
 
-#regarder .extend pour ajouter éléments a une liste
 def alarme(t):
     global heure_actuelle
-    liste_alarme=[]
-    for i in t:
-        liste_alarme +=[i]
+    liste_alarme=list(t)
     if liste_alarme == heure_actuelle:
         print("Il est l'or mon seignor!")
-
-
 
 heures=0
 minutes=0
 secondes=0
-tuple_heure = (15, 40, 30)
-tuple_alarme = (15, 40, 35)
-affichage = True
-
+print("pour mettre l'horloge en pause appuyez sur espace, pour la relancer appuyez sur 'p'")
+tuple_heure = tuple(map(int, input("entrer une heure: heures, minutes, secondes ").split())) # convertir les inputs en int avec la fonction map et int, puis les convertir en tuple
+tuple_alarme = tuple(map(int, input("entrer une heure pour l'alarme ").split()))             # syntaxe de map : map(fonction, iterable) applique la fonction a tout les iterables
+affichage_12 = False
 
 afficher_heure(tuple_heure)
 while True:
@@ -36,10 +32,10 @@ while True:
     if minutes > 59:
         minutes=0
         heures+=1
-    if heures > 24:
+    if heures > 23:
         heures = 0
     heure_actuelle = [heures, minutes, secondes]
-    if affichage:
+    if affichage_12:
         if heures < 12:
             print(f"{heures:02d}:{minutes:02d}:{secondes:02d} AM", end="\r")
         else:
@@ -48,10 +44,9 @@ while True:
     else:
         print(f"{heures:02d}:{minutes:02d}:{secondes:02d}", end="\r")
         alarme(tuple_alarme)
-    if #touche P préssé:
-        while true:
-            if # touche r préssé:
+    if keyboard.is_pressed(" "):
+        while True:
+            if keyboard.is_pressed("p"):
                 break
     time.sleep(1)
-    
 
